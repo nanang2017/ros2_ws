4c3936390a0042a07aef1fef1c0d6b995241cddf
이인환 <lih96042015@gmail.com>
2025-02-01 18:40:07 +0900

    ,

---
+++ src/my_robot/src/ridar/setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import setup
+package_name = 'ridar'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hwan',
+    maintainer_email='lih96042015@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
